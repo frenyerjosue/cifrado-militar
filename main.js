b76c9f6e98f6883399ee8d345416221a35368463
frenyerjosue <frenyerzambrano@gmail.com>
2026-05-09 14:38:48 -0400

    Notificaciones de actualizacion

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog} = require('electron');
 const path = require('path');
 const { machineIdSync } = require('node-machine-id');
 const { autoUpdater } = require('electron-updater');
@@ -51,6 +51,37 @@ autoUpdater.on('update-available', () => {
 autoUpdater.on('update-downloaded', () => {
     console.log('Descarga terminada. Se instalará al cerrar la app.');
     // También podrías enviar un mensaje al frontend para mostrarle un botón de "Reiniciar ahora"
+});
+
+autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'ENCRIPTS - Sistema de Actualización',
+        message: 'Descargando nuevas actualizaciones en segundo plano',
+        buttons: ['Entendido']
+    });
+});
+
+// 2. Cuando la descarga del .exe ha finalizado y está lista para instalarse
+autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+        type: 'question',
+        title: 'Actualización Lista',
+        message: 'Las actualizaciones han sido descargadas con éxito.\n\n¿Desea reiniciar el sistema para aplicarlas ahora?',
+        buttons: ['Reiniciar Ahora', 'Más Tarde'],
+        defaultId: 0, // El botón resaltado por defecto
+        cancelId: 1   // Qué hace si cierran la ventana con la X
+    }).then((result) => {
+        // Si el usuario hace clic en el botón 0 ('Reiniciar Ahora')
+        if (result.response === 0) {
+            autoUpdater.quitAndInstall(); // Cierra la app e instala la nueva versión
+        }
+    });
+});
+
+// (Opcional pero recomendado) Por si ocurre algún corte de internet durante la descarga
+autoUpdater.on('error', (error) => {
+    console.error('Error en el actualizador:', error);
 });
 
 // --- PROTOCOLO DE INTERCOMUNICACIÓN (BACKEND) ---
